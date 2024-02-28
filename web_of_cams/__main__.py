edec28ebdf79e0b48d922b8fee56b40d1a4aeff9
philipqueen <queen.philip@gmail.com>
2024-02-27 20:01:44 -0700

    adapt main.py

--- web_of_cams/__main__.py
+++ web_of_cams/__main__.py
@@ -1,19 +1,32 @@
+import sys
+import time
+from PySide6.QtWidgets import QApplication
 from multiprocessing import Event
 
 from web_of_cams.camera_frame_buffer import CameraFrameBuffer
 from web_of_cams.detect_cameras import detect_cameras
-from web_of_cams.process_handler import camera_process_handler_sm
+from web_of_cams.process_handler import camera_process_handler_sm, shutdown_processes
 
 
-def main():
+def setup_cameras() -> list[CameraFrameBuffer]:
     webcam_info = detect_cameras()
-    stop_event = Event()
     camera_buffers: list[CameraFrameBuffer] = []
     for webcam_id, frame_size in webcam_info.items():
         buffer = CameraFrameBuffer(webcam_id, frame_size)
         camera_buffers.append(buffer)
-    camera_process_handler_sm(camera_buffers, stop_event)
 
+    return camera_buffers
+
+
+def main():
+    stop_event = Event()
+
+    camera_buffers = setup_cameras()
+
+    processes = camera_process_handler_sm(camera_buffers, stop_event)
+
+    time.sleep(2)
+    shutdown_processes(processes, camera_buffers, stop_event)
 
 if __name__ == "__main__":
     main()
